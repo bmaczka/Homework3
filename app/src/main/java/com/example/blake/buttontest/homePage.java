ee621277023f956c39dcd44f9da3ea1ae3d7f15f
bmaczka <bmaczka@umich.edu>
2018-10-30 18:45:02 -0400

    Adding menu bar for homework 3

--- app/src/main/java/com/example/blake/buttontest/homePage.java
+++ app/src/main/java/com/example/blake/buttontest/homePage.java
@@ -1,7 +1,11 @@
 package com.example.blake.buttontest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class homePage extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class homePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater optionsMenuInflator = getMenuInflater();
+        optionsMenuInflator.inflate(R.menu.dropdown_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.main_activity_menu_item:
+                Intent mainActivityIntent = new Intent(homePage.this, MainActivity.class);
+                startActivity(mainActivityIntent);
+                return true;
+            case R.id.home_activity_menu_item:
+                return true;
+            case R.id.test_activity_menu_item:
+                Intent testActivityIntent = new Intent(homePage.this, test_Page.class);
+                startActivity(testActivityIntent);
+                return true;
+
+            default:
+                return true;
+        }
     }
 }
--- app/src/main/java/com/example/blake/buttontest/test_Page.java
+++ app/src/main/java/com/example/blake/buttontest/test_Page.java
@@ -1,7 +1,11 @@
 package com.example.blake.buttontest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class test_Page extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class test_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test__page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater optionsMenuInflator = getMenuInflater();
+        optionsMenuInflator.inflate(R.menu.dropdown_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.main_activity_menu_item:
+                Intent mainActivityIntent = new Intent(test_Page.this, MainActivity.class);
+                startActivity(mainActivityIntent);
+                return true;
+            case R.id.home_activity_menu_item:
+                Intent testActivityIntent = new Intent(test_Page.this, homePage.class);
+                startActivity(testActivityIntent);
+                return true;
+            case R.id.test_activity_menu_item:
+                return true;
+
+            default:
+                return true;
+        }
     }
 }
